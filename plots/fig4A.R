fccfd1bbbc480c087e9b61535168af08463a393a
David Little <david.frank.little@gmail.com>
2018-07-10 10:34:58 -0600

    Updated fig 4A

--- plots/fig4A.R
+++ plots/fig4A.R
@@ -1,30 +1,30 @@
 library(ggplot2)
 
-source(file.path('preprocessing','files.txt'))
+source(file.path("preprocessing","files.txt"))
 
-df = read.csv(file.path("preprocessing","data",fig4A_data))
-model = read.csv(file.path("preprocessing","data",fig4A_model))
+df = read.csv(file.path("preprocessing","data",fig5A_data))
+model = read.csv(file.path("preprocessing","data",fig5A_model))
 
 pdodge = position_dodge(width=0.8)
-pl = ggplot(model,aes(x=regimen,y=mean*100)) 
-pl = pl + geom_bar(stat='identity',data=model,
-                   position=pdodge,fill='lightgrey',width=0.6)
+pl = ggplot(model,aes(x=regimen,y=mean*100))
+pl = pl + geom_bar(stat="identity",data=model,
+                   position=pdodge,fill="lightgrey",width=0.6)
 pl = pl + geom_linerange(data=model,position=pdodge,
                          aes(ymin=lower*100,ymax=upper*100))
 pl = pl + geom_point(data=df,position=pdodge)
 pl = pl + scale_y_continuous(breaks=seq(20,100,10))
 pl = pl + geom_hline(yintercept=25,linetype=2)
-pl = pl + xlim(c('AP','LA','SA','P'))
+pl = pl + xlim(c("AP","LA","SA","P"))
 pl = pl + coord_cartesian(xlim=c(0.5,4.5),ylim=c(20,100),expand=F)
 pl = pl + theme_classic()
-pl = pl + xlab('% correct classification')
+pl = pl + xlab("% correct classification")
 
 
-file = file.path('plots','pdfs',paste('fig4A_',Sys.Date(),'.pdf',sep=''))
+file = file.path("plots","pdfs",paste("fig5A_",Sys.Date(),".pdf",sep=""))
 ggsave(file,pl,width=8,height=6,useDingbats=FALSE)
 
-cat('Created file ')
+cat("Created file ")
 cat(file)
-cat('\n')
+cat("\n")
 
 
--- preprocessing/fig4A.py
+++ preprocessing/fig4A.py
@@ -1,26 +1,28 @@
 import os.path as op
-execfile(op.join('util','header.py'))
+execfile(op.join('util', 'header.py'))
 
 dt = datetime.date.today().strftime("%Y-%m-%d")
 
 lc = setup.label_counts
-model_samples = op.join("preprocessing","data","fig4A_samples_"+dt+".h5")
+model_samples = op.join("preprocessing", "data", "fig5A_samples_"+dt+".h5")
 if op.isfile(model_samples):
   os.remove(model_samples)
-model = regress.robit('mean ~ center(pretest) + regimen',lc,
-                      error_prior=lc['len'].max(),cache_file=model_samples)
+
+model = regress.robit('mean ~ center(pretest) + regimen', lc,
+                      error_prior=lc['len'].max(), cache_file=model_samples)
 print "Created "+model_samples
 
 d = lc.groupby(['regimen']).pretest.mean().to_frame('pretest').reset_index()
-p = model.predict(d,use_dataframe=True)
-predict = p.groupby(['regimen']).y.apply(ss.coef_stats_ns).unstack(level=1).reset_index()
+p = model.predict(d, use_dataframe=True)
+predict = (p.groupby(['regimen']).y.apply(ss.coef_stats_ns).
+           unstack(level=1).reset_index())
 
-data_file = op.join("preprocessing","data","fig4A_data_"+dt+".csv")
-lc.to_csv(data_file,header=True)
+data_file = op.join("preprocessing", "data", "fig5A_data_"+dt+".csv")
+lc.to_csv(data_file, header=True)
 print "Created "+data_file
 
-model_file = op.join("preprocessing","data","fig4A_model_"+dt+".csv")
-predict.to_csv(model_file,header=True)
+model_file = op.join("preprocessing", "data", "fig5A_model_"+dt+".csv")
+predict.to_csv(model_file, header=True)
 print "Created "+model_file
 
 print "Please manually update preprocessing/files.txt"
