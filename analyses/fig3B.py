4cf8d2cf59174da57bbba31ca870f0af2e89bcb1
David Little <david.frank.little@gmail.com>
2018-07-16 11:49:50 -0600

    Updated 3B to 4A

--- analyses/fig3B.py
+++ analyses/fig3B.py
@@ -1,15 +1,16 @@
 import os.path as op
-execfile(op.join('util','header.py'))
-execfile(op.join('preprocessing','files.txt'))
+execfile(op.join('util', 'header.py'))
+execfile(op.join('preprocessing', 'files.txt'))
 
-c = (setup.counts.query('condition == ["4th","4th_gen"] and day == 4').
-     groupby(['regimen','sid','condition','meanpre'])['mean'].
+c = (setup.counts.query('condition == ["4th", "4th_gen"] and day == 4').
+     groupby(['regimen', 'sid', 'condition', 'meanpre'])['mean'].
      agg(np.mean).unstack('condition').reset_index())
 
-c.rename(columns={'4th': 'trained', '4th_gen': 'untrained'},inplace=True)
+c.rename(columns={'4th': 'trained', '4th_gen': 'untrained'}, inplace=True)
 
-model = regress.robit("untrained ~ trained",c,error_prior=100,
-                      cache_file=op.join("preprocessing","data",fig3B_samples))
+model = regress.robit("untrained ~ trained", c, error_prior=100,
+                      cache_file=op.join("preprocessing", "data",
+                                         fig4B_samples))
 
 print "Analysis of trained vs. untrained"
 print "----------------------------------------"
@@ -18,16 +19,16 @@ print model.validate()
 
 print "----------------------------------------"
 print "Model stats"
-print "R^2: %4.3f (SE = %3.3f)" % (np.mean(model.R2()),np.std(model.R2()))
+print "R^2: %4.3f (SE = %3.3f)" % (np.mean(model.R2()), np.std(model.R2()))
 print "p-value: %4.3f" % (ss.p_value(model.R2()))
 
 print "----------------------------------------"
 print "Does regimen infuence trained vs. untrained analysis?"
 
-model_reg = regress.robit("untrained ~ trained*regimen",c,
-                           error_prior=100,
-                          cache_file=op.join("preprocessing","data",
-                                             fig3B_samples_byregimen))
+model_reg = regress.robit("untrained ~ trained*regimen", c,
+                          error_prior=100,
+                          cache_file=op.join("preprocessing", "data",
+                                             fig4B_samples_byregimen))
 
 print "----------------------------------------"
 print "model validation"
@@ -36,24 +37,26 @@ print model_reg.validate()
 print "----------------------------------------"
 print "differences in trained vs. untrained by regimen:"
 p = pd.DataFrame([{'regimen': regimen, 'trained': model_reg.df.trained.mean()}
-                   for regimen in model_reg.df.regimen.unique()])
-pdf = model_reg.predict(p,use_dataframe=True).set_index(['regimen','sample']).y
+                  for regimen in model_reg.df.regimen.unique()])
+pdf = (model_reg.predict(p, use_dataframe=True).
+       set_index(['regimen', 'sample']).y)
 pdf = pdf.unstack('regimen')
-print ss.contrast_table(pdf*100,pdf.columns)
+print ss.contrast_table(pdf*100, pdf.columns)
 
 
 
 print "----------------------------------------"
 print "Does regimen infuence trained vs. untrained analysis?"
 
-cf = (setup.counts.query('condition == ["4th","4th_gen"] and day == 4').
-     groupby(['regimen','sid','condition','meanpre','foil_label'])['mean'].
-     agg(np.mean).unstack('condition').reset_index())
-cf.rename(columns={'4th': 'trained', '4th_gen': 'untrained'},inplace=True)
-model_foil = regress.robit("untrained ~ trained * foil_label",cf,
+cf = (setup.counts.query('condition == ["4th", "4th_gen"] and day == 4').
+      groupby(['regimen', 'sid', 'condition', 'meanpre',
+               'foil_label'])['mean'].
+      agg(np.mean).unstack('condition').reset_index())
+cf.rename(columns={'4th': 'trained', '4th_gen': 'untrained'}, inplace=True)
+model_foil = regress.robit("untrained ~ trained * foil_label", cf,
                            error_prior=100,
-                           cache_file=op.join("preprocessing","data",
-                                              fig3B_samples_byfoil))
+                           cache_file=op.join("preprocessing", "data",
+                                              fig4B_samples_byfoil))
 
 print "----------------------------------------"
 print "validation"
@@ -63,8 +66,8 @@ print "----------------------------------------"
 print "differences in trained vs. untrained by foil:"
 p = pd.DataFrame([{'trained': model_foil.df.trained.mean(),
                    'foil_label': foil_label}
-                   for foil_label in model_foil.df.foil_label.unique()])
-pdf = (model_foil.predict(p,use_dataframe=True).
-       set_index(['foil_label','sample']).y)
+                  for foil_label in model_foil.df.foil_label.unique()])
+pdf = (model_foil.predict(p, use_dataframe=True).
+       set_index(['foil_label', 'sample']).y)
 pdf = pdf.unstack('foil_label')
-print ss.contrast_table(pdf*100,pdf.columns)
+print ss.contrast_table(pdf*100, pdf.columns)
--- preprocessing/fig3B.py
+++ preprocessing/fig3B.py
@@ -9,24 +9,26 @@ c = (setup.counts.query('condition == ["4th","4th_gen"] and day == 4').
 
 c.rename(columns={'4th': 'trained', '4th_gen': 'untrained'},inplace=True)
 
-model_samples = op.join("preprocessing","data","fig3B_samples_"+dt+".h5")
+model_samples = op.join("preprocessing","data","fig4B_samples_"+dt+".h5")
 if op.isfile(model_samples):
   os.remove(model_samples)
 model = regress.robit("untrained ~ trained",c,error_prior=100,
                       cache_file=model_samples)
 
-model_reg_samples = op.join("preprocessing","data","fig3B_samples_byregimen_"+dt+".h5")
+model_reg_samples = op.join("preprocessing","data",
+                            "fig4B_samples_byregimen_"+dt+".h5")
 if op.isfile(model_reg_samples):
   os.remove(model_reg_samples)
 model_reg = regress.robit("untrained ~ trained*regimen",c,
                           error_prior=100,cache_file=model_reg_samples)
 
-model_foil_samples = op.join("preprocessing","data","fig3B_samples_byfoil_"+dt+".h5")
+model_foil_samples = op.join("preprocessing","data",
+                             "fig4B_samples_byfoil_"+dt+".h5")
 if op.isfile(model_foil_samples):
   os.remove(model_foil_samples)
 cf = (setup.counts.query('condition == ["4th","4th_gen"] and day == 4').
-     groupby(['regimen','sid','condition','meanpre','foil_label'])['mean'].
-     agg(np.mean).unstack('condition').reset_index())
+      groupby(['regimen','sid','condition','meanpre','foil_label'])['mean'].
+      agg(np.mean).unstack('condition').reset_index())
 cf.rename(columns={'4th': 'trained', '4th_gen': 'untrained'},inplace=True)
 model_foil = regress.robit("untrained ~ trained * foil_label",
                            cf,error_prior=100,cache_file=model_foil_samples)
@@ -36,16 +38,17 @@ print "Created "+model_samples
 print "Created "+model_reg_samples
 print "Created "+model_foil_samples
 
-d = pd.DataFrame({'trained': np.linspace(0.4,1,50), 'meanpre': model.df.meanpre.mean()})
+d = pd.DataFrame({'trained': np.linspace(0.4,1,50),
+                  'meanpre': model.df.meanpre.mean()})
 rmodel = (model.predict(d,use_dataframe=True).groupby('trained').y.
           apply(ss.coef_stats_ns).unstack(level=1).reset_index())
 rdata = model.df
 
-data_file = op.join("preprocessing","data","fig3B_data_"+dt+".csv")
+data_file = op.join("preprocessing","data","fig4B_data_"+dt+".csv")
 rdata.to_csv(data_file,header=True)
 print "Created "+data_file
 
-model_file = op.join("preprocessing","data","fig3B_model_"+dt+".csv")
+model_file = op.join("preprocessing","data","fig4B_model_"+dt+".csv")
 rmodel.to_csv(model_file,header=True)
 print "Created "+model_file
 
