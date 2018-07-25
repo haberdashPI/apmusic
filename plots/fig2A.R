8693db3dbd8dd9209c5d929c24716e7c21a3583d
David Little <david.frank.little@gmail.com>
2018-07-25 10:11:09 -0400

    Renamed 1A-C -> 2A-C

--- plots/fig2A.R
+++ plots/fig2A.R
@@ -1,30 +1,23 @@
-library(ggplot2)
-require(Hmisc)
-
+require(ggplot2)
 source(file.path("preprocessing","files.txt"))
+rmodel = read.csv(file.path("preprocessing","data",fig2A_model))
+rdata = read.csv(file.path("preprocessing","data",fig2A_data))
 
-df = read.csv(file.path("preprocessing","data",fig3A_data))
-model = read.csv(file.path("preprocessing","data",fig3A_model))
+pl = ggplot(rmodel,aes(y=mean,x=foil_label))
+pl = pl + geom_bar(stat='identity',fill='gray')
+pl = pl + geom_linerange(aes(ymin=lower,ymax=upper))
+pl = pl + geom_point(data=rdata,mapping=aes(y=(correct+0.5)/(total+1)),
+                     position=position_jitter(width=0.05))
+pl = pl + xlim(c('3rd','5th','6th')) + xlab('Foil')
+pl = pl + theme_classic()
+pl = pl + scale_y_continuous(name="Day 1 % Correct",
+                             breaks=seq(0.4,1,0.1),
+                             labels=100*seq(0.4,1,0.1))
+pl = pl + coord_cartesian(ylim=c(0.4,1))
 
-pdodge = position_dodge(width=0.2)
-p = ggplot(df,aes(x=day,y=mean,group=regimen,shape=regimen,fill=regimen))
-p = p + geom_smooth(data=model,stat="identity",
-                    aes(ymin=cilower,ymax=ciupper,linetype=regimen),
-                    fill="grey",color="black")
-p = p + stat_summary(fun.data="mean_cl_boot",position=pdodge,
-                     fun.args=list(conf.int=0.682))
-p = p + theme_classic()
-p = p + scale_fill_manual(values=c("white","gray","gray","black"),
-                          limits=c("P","SA","LA","AP"))
-p = p + scale_shape_manual(limits=c("P","SA", "LA", "AP"),
-                           values=c(21,21,22,22))
-p = p + scale_linetype_manual(limits=c("P","SA", "LA", "AP"),
-                              values=c(3,2,1,1))
-p
+file = file.path('plots','pdfs',paste('fig2A_',Sys.Date(),'.pdf',sep=''))
+ggsave(file,pl,width=5.5,height=3.5,useDingbats=FALSE)
 
-file = file.path("plots","pdfs",paste("fig3A_",Sys.Date(),".pdf",sep=""))
-ggsave(file,p,width=8,height=6,useDingbats=FALSE)
-
-cat("Created file ")
+cat('Created file ')
 cat(file)
-cat("\n")
+cat('\n')
