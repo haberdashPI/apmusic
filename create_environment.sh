2b47f56dc901302e3aafdb4e69d52569e71fd5b4
David Little <david.frank.little@gmail.com>
2018-07-25 11:13:08 -0400

    Verified enviornment

--- create_environment.sh
+++ create_environment.sh
@@ -4,10 +4,11 @@
 #
 # BEWARE: you'll quite possibly end up with newer versions of
 # the software, so this may break the code in small ways.
-# (Usually this will invovle reading error messages and fixing
+# (Usually this will invovle reading  a few error messages and fixing
 # the code appropriately)
 
 
-conda create --channel conda-forge --override-channels --name apmusic_2018_03_02 python=2.7
-source activate apmusic_2018_03_02
-conda install -c conda-forge r-base r-tidyr r-dplyr r-ggplot2 pystan r-cowplot r-stringr pymc3 scipy numpy pandas ipython h5py 
+conda create --channel conda-forge --override-channels --name apmusic_2018_07_24 python=2.7
+source activate apmusic_2018_07_24
+conda install -c conda-forge r-base r-tidyr r-dplyr r-ggplot2 pystan r-cowplot r-stringr pymc3 scipy numpy pandas ipython h5py appdirs statsmodels xlrd r-hmisc r-lattice
+
